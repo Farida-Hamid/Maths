509097fffcd8ed9844cba5b311f337fd2b4064ec
Farida-Hamid <faridaaddala@gmail.com>
2022-09-14 08:53:35 +0200

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Math from './components/Math';
